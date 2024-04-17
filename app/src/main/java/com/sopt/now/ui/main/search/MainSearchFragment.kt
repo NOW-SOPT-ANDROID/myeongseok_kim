1daf3343b3d33faf7869999ad1e29bc5c5b3d22f
김명석 <twinaudtjr@naver.com>
2024-04-18 04:31:14 +0900

    [chore] #4 필요없는 코드 삭제

--- app/src/main/java/com/sopt/now/ui/main/search/MainSearchFragment.kt
+++ app/src/main/java/com/sopt/now/ui/main/search/MainSearchFragment.kt
@@ -2,33 +2,16 @@ package com.sopt.now.ui.main.search
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.activityViewModels
 import com.sopt.now.R
 import com.sopt.now.databinding.FragmentMainProfileBinding
-import com.sopt.now.ui.main.MainViewModel
-import com.sopt.now.ui.main.home.MainHomeAdapter
 import com.sopt.now.util.BindingFragment
 
 class MainSearchFragment :
     BindingFragment<FragmentMainProfileBinding>(R.layout.fragment_main_search) {
-    private val viewModel: MainViewModel by activityViewModels()
-    private lateinit var mainHomeAdapter: MainHomeAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initLayout()
-        setUserList()
     }
 
-    private fun initLayout() {
-        //추후 추가
-    }
-
-    private fun setUserList() {
-        //todo 뷰모델 활용한 검색기능
-        viewModel.userData.observe(viewLifecycleOwner) {
-            mainHomeAdapter.setUserList(it)
-        }
-    }
 
 }

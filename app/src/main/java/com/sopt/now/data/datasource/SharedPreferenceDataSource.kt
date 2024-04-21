7fb00f3efb5cc193d59cf6c2cacb1b490bf567f0
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-21 18:09:01 +0900

    [add] UserDto 추가

--- app/src/main/java/com/sopt/now/data/datasource/SharedPreferenceDataSource.kt
+++ app/src/main/java/com/sopt/now/data/datasource/SharedPreferenceDataSource.kt
@@ -1,7 +1,9 @@
 package com.sopt.now.data.datasource
 
+import com.sopt.now.data.dto.UserDto
+
 interface SharedPreferenceDataSource {
-    fun saveInfo()
-    fun getInfo()
+    fun saveInfo(userDto: UserDto)
+    fun getInfo(): UserDto
     fun clear()
 }
---
+++ app/src/main/java/com/sopt/now/data/dto/UserDto.kt
@@ -0,0 +1,8 @@
+package com.sopt.now.data.dto
+
+data class UserDto(
+    val id: String,
+    val password: String,
+    val nickname: String,
+    val mbti: String
+)

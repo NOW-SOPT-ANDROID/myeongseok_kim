5db0e00d8c5d561b2c2e8c6e3089efea2ccb5486
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-21 19:48:31 +0900

    [add] 에러 메시지 담당하는 ErrorMessage 추가

---
+++ app/src/main/java/com/sopt/now/feature/ErrorMessage.kt
@@ -0,0 +1,8 @@
+package com.sopt.now.feature
+
+object ErrorMessage {
+    const val SIGN_UP_REQUIRED = "회원 정보가 없습니다. 회원 가입을 해주세요"
+    const val ID_PASSWORD_REQUIRED = "아이디와 비밀번호 입력해주세요"
+    const val INVALID_ID = "아이디가 일치하지 않습니다."
+    const val INVALID_PASSWORD = "비밀번호가 일치하지 않습니다."
+}

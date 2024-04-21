bb59998ab3c5325fa58b224945c88e2bd0d7abdd
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-04-21 18:13:29 +0900

    [add] UserEntity 추가

--- app/src/main/java/com/sopt/now/data/dto/UserDto.kt
+++ app/src/main/java/com/sopt/now/data/dto/UserDto.kt
@@ -1,8 +1,18 @@
 package com.sopt.now.data.dto
 
+import com.sopt.now.domain.entity.UserEntity
+
 data class UserDto(
     val id: String,
     val password: String,
     val nickname: String,
     val mbti: String
 )
+{
+    fun toUserEntity() = UserEntity(
+        id = id,
+        password = password,
+        nickname = nickname,
+        mbti = mbti
+    )
+}

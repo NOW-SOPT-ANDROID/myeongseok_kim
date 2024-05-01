9fdf6d6c483d7eecbc7d46b3db91ddf28bf540d9
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-05-01 13:21:15 +0900

    Revert "Feat/week 02 compose"

--- app/src/main/java/com/sopt/now/compose/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.sopt.now.compose.navigation
-
-sealed class Screen(val route: String) {
-    data object Login : Screen("login")
-    data object Home : Screen("home")
-    data object SignUp : Screen("signup")
-}

b589142e63c8e8d004f2648a95b60c4eea0af756
김명석 <twinaudtjr@naver.com>
2024-05-02 18:34:23 +0900

    [add] #10 MainViewModel 추가

--- app/src/main/java/com/sopt/now/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sopt/now/ui/main/MainViewModel.kt
@@ -3,14 +3,53 @@ package com.sopt.now.ui.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sopt.now.data.Profile
+import com.sopt.now.data.ServicePool.infoService
 import com.sopt.now.data.User
+import com.sopt.now.data.datasouce.RequestLoginDto
+import com.sopt.now.data.datasouce.ResponseInfoDto
+import com.sopt.now.data.datasouce.ResponseLoginDto
+import com.sopt.now.util.UiState
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
     private val _userData = MutableLiveData<List<Profile>>()
     val userData = _userData
 
+    val myInfo = MutableLiveData<UiState<User>>()
+
     private val _myProfile = MutableLiveData<User>()
     val myProfile = _myProfile
+
+
+    fun getInfo(userid:String) {
+        infoService.getUserInfo(userid).enqueue(object : Callback<ResponseInfoDto> {
+            override fun onResponse(
+                call: Call<ResponseInfoDto>,
+                response: Response<ResponseInfoDto>,
+            ) {
+                if (response.isSuccessful) {
+                    val user = response.body()?.data
+//                    myInfo.value = UiState.Success(response.body().data.toUser())
+                } else {
+                    val error = response.errorBody()?.string()
+                    try {
+                        val errorJson = JSONObject(error)
+                        val errorMessage = errorJson.getString("message")
+                        myInfo.value = UiState.Error(errorMessage)
+                    } catch (e: Exception) {
+                        myInfo.value = UiState.Error("로그인 실패  에러 메시지 파싱 실패")
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseInfoDto>, t: Throwable) {
+                myInfo.value = UiState.Error("서버 에러")
+            }
+        })
+    }
 
     init {
         _userData.value =

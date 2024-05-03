9861bf7c97c84207fa3d269fde716168fe9bfb8a
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-05-03 20:16:32 +0900

    [add] #12 login 관련 서버 통신 추가

--- app/src/main/java/com/sopt/now/compose/data/AuthService.kt
+++ app/src/main/java/com/sopt/now/compose/data/AuthService.kt
@@ -1,6 +1,8 @@
 package com.sopt.now.compose.data
 
+import com.sopt.now.compose.data.datasource.request.RequestLoginDto
 import com.sopt.now.compose.data.datasource.request.RequestSignUpDto
+import com.sopt.now.compose.data.datasource.response.ResponseLoginDto
 import com.sopt.now.compose.data.datasource.response.ResponseSignUpDto
 import retrofit2.Call
 import retrofit2.http.Body
@@ -11,4 +13,9 @@ interface AuthService {
     fun signUp(
         @Body request: RequestSignUpDto,
     ): Call<ResponseSignUpDto>
+
+    @POST("member/login")
+    fun login(
+        @Body request: RequestLoginDto,
+    ): Call<ResponseLoginDto>
 }

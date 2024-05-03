f71d301e64c01b1426cce76a4fdc24a17179576a
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-05-03 20:16:19 +0900

    [mod] #12 toastMessage 변경

--- app/src/main/java/com/sopt/now/compose/component/ContextExt.kt
+++ app/src/main/java/com/sopt/now/compose/component/ContextExt.kt
@@ -3,6 +3,6 @@ package com.sopt.now.compose.component
 import android.content.Context
 import android.widget.Toast
 
-fun Context.toastMessage(message: Int) {
+fun Context.toastMessage(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }

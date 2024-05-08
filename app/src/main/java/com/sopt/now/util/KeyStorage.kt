d7639af44a0d9d06fb1d59775fc22dac49570d93
김명석 <twinaudtjr@naver.com>
2024-05-09 01:59:02 +0900

    [add] #10 KeyStorage 추가

---
+++ app/src/main/java/com/sopt/now/util/KeyStorage.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.util
+
+object  KeyStorage {
+}
+
+object StringNetworkError{
+    const val SERVER_ERROR ="서버 에러"
+    const val FAIL_ERROR = "%s 실패 : 에러 메시지 파싱 실패"
+
+    const val LOGIN = "로그인"
+    const val SIGNUP = "회원가입"
+}
+

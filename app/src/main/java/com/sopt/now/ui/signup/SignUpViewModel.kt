c3591ce518696bfd9e69af10d7871c0d56c932bd
김명석 <twinaudtjr@naver.com>
2024-06-04 02:29:12 +0900

    [mod] #18 SignUpViewModel 예외처리 변경

--- app/src/main/java/com/sopt/now/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/sopt/now/ui/signup/SignUpViewModel.kt
@@ -3,18 +3,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.data.api.ServicePool
 import com.sopt.now.data.datasouce.request.RequestSignUpDto
-import com.sopt.now.data.datasouce.response.BaseResponse
 import com.sopt.now.data.model.User
-import com.sopt.now.util.StringNetworkError.FAIL_ERROR
-import com.sopt.now.util.StringNetworkError.SERVER_ERROR
-import com.sopt.now.util.StringNetworkError.SIGNUP
 import com.sopt.now.util.UiState
 import kotlinx.coroutines.launch
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.HttpException
-import retrofit2.Response
 
 class SignUpViewModel : ViewModel() {
     private val authService by lazy { ServicePool.authService }
@@ -27,18 +20,21 @@ class SignUpViewModel : ViewModel() {
         viewModelScope.launch {
             runCatching {
                 authService.signUp(request)
-            }.onSuccess {
-                _signUpState.value =
-                    UiState.Success(request.toUserWithUserId(it.headers()["userid"].toString()))
+            }.onSuccess { response ->
+                if (response.isSuccessful)
+                    _signUpState.value =
+                        UiState.Success(request.toUserWithUserId(response.headers()["userid"].toString()))
+                else {
+                    val errorMessage =
+                        JSONObject(response.errorBody()?.string()).getString("message")
+                    _signUpState.value = UiState.Error(errorMessage.toString())
+                }
             }.onFailure { e ->
                 if (e is HttpException) {
-                    val errorMessage =
-                        JSONObject(e.response()?.errorBody()?.string()).getString("message")
-                    _signUpState.value = UiState.Error(errorMessage)
+                    _signUpState.value = UiState.Error(e.message())
                 } else {
-                    _signUpState.value = UiState.Error("코드 똑바로 짜라.. ")
+                    _signUpState.value = UiState.Error(e.message.toString())
                 }
-
             }
         }
     }

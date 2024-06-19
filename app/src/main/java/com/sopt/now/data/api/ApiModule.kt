0c6955418a36abb12382856bea3f995723b7af11
김명석 <75840431+cacaocoffee@users.noreply.github.com>
2024-06-19 16:18:34 +0900

    [Add] #18 Api, DataModule 추가

---
+++ app/src/main/java/com/sopt/now/data/api/ApiModule.kt
@@ -0,0 +1,33 @@
+package com.sopt.now.data.api
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.sopt.now.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    private const val CONTENT_TYPE = "application/json"
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.AUTH_BASE_URL)
+            .addConverterFactory(Json.asConverterFactory(CONTENT_TYPE.toMediaType()))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthService(retrofit: Retrofit): AuthService {
+        return retrofit.create(AuthService::class.java)
+    }
+}
